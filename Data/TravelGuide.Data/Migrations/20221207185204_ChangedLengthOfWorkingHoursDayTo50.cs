e8e24d2b6eef2be08590445edb5da8af5cce2c05
yoantodorov <todorovyoan075@gmail.com>
2022-12-07 22:04:41 +0200

    Added migration.
    Changed length of workingHours day to 50.

---
+++ Data/TravelGuide.Data/Migrations/20221207185204_ChangedLengthOfWorkingHoursDayTo50.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelGuide.Data.Migrations
+{
+    public partial class ChangedLengthOfWorkingHoursDayTo50 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "WeekDay",
+                table: "WorkingHours",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "WeekDay",
+                table: "WorkingHours",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
